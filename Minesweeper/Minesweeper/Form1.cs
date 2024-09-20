0eed00357f2bd0b193a75586c5b379f0479ebd03
FlatAsAPancake <ajkou571@learner.dvusd.org>
2024-09-20 09:02:22 -0700

    Another Change forced upon my life in this god forsaken hellscape

--- Minesweeper/Minesweeper/Form1.cs
+++ Minesweeper/Minesweeper/Form1.cs
@@ -12,38 +12,49 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
-        Random random= new Random();
+        Random random = new Random();
         Button[] btnGrid = new Button[100];
         Tile[] tileGrid = new Tile[100];
         int flagcount = 0;
         public Form1()
         {
             InitializeComponent();
-
-            for (int i = 0; i < 100; i++)
-            {
-                btnGrid[i] = (Button)Controls["button" + (i + 1)];
-                tileGrid[i] = new Tile(btnGrid[i]);
-                btnGrid[i].BackColor= Color.Gray;
-
-            }
+            Reset();
         }
         private Button GetButton(int r, int c)
         {
             return (Button)GetButton(r, c);
-
         }
-        private int getIndex(Button B)
+        private int getIndex(Button b)
         {
-            string tmp = B.Name.Substring(6);
+            string tmp = b.Name.Substring(6);
             int retVal = 0;
             int.TryParse(tmp, out retVal);
-            return retVal = -1;
+            return retVal - 1;
         }
         private void button91_MouseDown(object sender, MouseEventArgs e)
         {
             Button b = sender as Button;
-            b.BackColor = Color.Red;
+            Tile t = tileGrid[getIndex(b)];
+            t.setFlag(true);
+
         }
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+        void Reset()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                btnGrid[i] = (Button)Controls["button" + (i + 1)];
+                tileGrid[i] = new Tile(btnGrid[i]);
+                tileGrid[i].setflagIMG(FlagBox.Image);
+                tileGrid[i].setMineIMG(MineBox.Image);
+                tileGrid[i].setFlag(true);
+                tileGrid[i].setMine(false);
+            }
+        }
+
     }
 }
--- Minesweeper/Minesweeper/Tile.cs
+++ Minesweeper/Minesweeper/Tile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,32 @@ namespace Minesweeper
 {
     internal class Tile
     {
-        Button m_b;
-        Boolean m_flag;
+        private Image m_flagIMG;
+        private Image m_mineIMG;
+        private Boolean m_flag;
+        private Boolean m_mine;
+        private Boolean m_dug;
+        private int m_nearby;
+        private Button m_b;
         public Tile(Button b)
         {
-            m_b= b;
+            m_b = b;
+            m_b.BackColor = Color.Gray;
         }
+        public void setNearBy(int n) {  m_nearby = n; }
+        public void setflagIMG(Image flagIMG) { m_flagIMG = flagIMG; }
+        public void setMineIMG(Image mineIMG) { m_mineIMG = mineIMG; }
+        public void setDug(Boolean b) { m_dug = b; }
+        public void setMine(Boolean b) 
+        {
+            m_mine = b;
+            m_b.BackgroundImage = m_mineIMG;
+        }
+        public void setFlag(Boolean b)
+        {
+            m_flag = b;
+            m_b.BackgroundImage = m_flagIMG;
+        }
+
     }
 }
