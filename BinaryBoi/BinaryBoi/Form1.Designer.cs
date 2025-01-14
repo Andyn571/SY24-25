226af6b1081cafa840de67ca79fb644781e2042e
FlatAsAPancake <ajkou571@learner.dvusd.org>
2025-01-14 08:35:40 -0700

    Mental Abuse To Humans

--- BinaryBoi/BinaryBoi/Form1.Designer.cs
+++ BinaryBoi/BinaryBoi/Form1.Designer.cs
@@ -34,67 +34,70 @@
             this.Bit2 = new System.Windows.Forms.TextBox();
             this.Bit1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.Bit8 = new System.Windows.Forms.TextBox();
+            this.Bit7 = new System.Windows.Forms.TextBox();
+            this.Bit6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Bit5
             // 
-            this.Bit5.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit5.Location = new System.Drawing.Point(93, 95);
+            this.Bit5.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit5.Location = new System.Drawing.Point(219, 117);
             this.Bit5.MaxLength = 1;
             this.Bit5.Multiline = true;
             this.Bit5.Name = "Bit5";
-            this.Bit5.Size = new System.Drawing.Size(100, 88);
+            this.Bit5.Size = new System.Drawing.Size(63, 64);
             this.Bit5.TabIndex = 0;
             this.Bit5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Bit5.TextChanged += new System.EventHandler(this.Bit5_TextChanged);
+            this.Bit5.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
             // Bit4
             // 
-            this.Bit4.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit4.Location = new System.Drawing.Point(199, 95);
+            this.Bit4.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit4.Location = new System.Drawing.Point(288, 117);
             this.Bit4.MaxLength = 1;
             this.Bit4.Multiline = true;
             this.Bit4.Name = "Bit4";
-            this.Bit4.Size = new System.Drawing.Size(100, 88);
+            this.Bit4.Size = new System.Drawing.Size(63, 64);
             this.Bit4.TabIndex = 1;
             this.Bit4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Bit4.TextChanged += new System.EventHandler(this.Bit5_TextChanged);
+            this.Bit4.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
             // Bit3
             // 
-            this.Bit3.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit3.Location = new System.Drawing.Point(305, 95);
+            this.Bit3.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit3.Location = new System.Drawing.Point(357, 117);
             this.Bit3.MaxLength = 1;
             this.Bit3.Multiline = true;
             this.Bit3.Name = "Bit3";
-            this.Bit3.Size = new System.Drawing.Size(100, 88);
+            this.Bit3.Size = new System.Drawing.Size(63, 64);
             this.Bit3.TabIndex = 2;
             this.Bit3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Bit3.TextChanged += new System.EventHandler(this.Bit5_TextChanged);
+            this.Bit3.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
             // Bit2
             // 
-            this.Bit2.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit2.Location = new System.Drawing.Point(411, 95);
+            this.Bit2.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit2.Location = new System.Drawing.Point(426, 117);
             this.Bit2.MaxLength = 1;
             this.Bit2.Multiline = true;
             this.Bit2.Name = "Bit2";
-            this.Bit2.Size = new System.Drawing.Size(100, 88);
+            this.Bit2.Size = new System.Drawing.Size(63, 64);
             this.Bit2.TabIndex = 3;
             this.Bit2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Bit2.TextChanged += new System.EventHandler(this.Bit5_TextChanged);
+            this.Bit2.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
             // Bit1
             // 
-            this.Bit1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit1.Location = new System.Drawing.Point(517, 95);
+            this.Bit1.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit1.Location = new System.Drawing.Point(495, 117);
             this.Bit1.MaxLength = 1;
             this.Bit1.Multiline = true;
             this.Bit1.Name = "Bit1";
-            this.Bit1.Size = new System.Drawing.Size(100, 88);
+            this.Bit1.Size = new System.Drawing.Size(63, 64);
             this.Bit1.TabIndex = 4;
             this.Bit1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.Bit1.TextChanged += new System.EventHandler(this.Bit5_TextChanged);
+            this.Bit1.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
             // label1
             // 
@@ -105,11 +108,50 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "label1";
             // 
+            // Bit8
+            // 
+            this.Bit8.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit8.Location = new System.Drawing.Point(12, 117);
+            this.Bit8.MaxLength = 1;
+            this.Bit8.Multiline = true;
+            this.Bit8.Name = "Bit8";
+            this.Bit8.Size = new System.Drawing.Size(63, 64);
+            this.Bit8.TabIndex = 6;
+            this.Bit8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bit8.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit7
+            // 
+            this.Bit7.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit7.Location = new System.Drawing.Point(81, 117);
+            this.Bit7.MaxLength = 1;
+            this.Bit7.Multiline = true;
+            this.Bit7.Name = "Bit7";
+            this.Bit7.Size = new System.Drawing.Size(63, 64);
+            this.Bit7.TabIndex = 7;
+            this.Bit7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bit7.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
+            // Bit6
+            // 
+            this.Bit6.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bit6.Location = new System.Drawing.Point(150, 117);
+            this.Bit6.MaxLength = 1;
+            this.Bit6.Multiline = true;
+            this.Bit6.Name = "Bit6";
+            this.Bit6.Size = new System.Drawing.Size(63, 64);
+            this.Bit6.TabIndex = 8;
+            this.Bit6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bit6.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Bit6);
+            this.Controls.Add(this.Bit7);
+            this.Controls.Add(this.Bit8);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Bit1);
             this.Controls.Add(this.Bit2);
@@ -131,6 +173,9 @@
         private System.Windows.Forms.TextBox Bit2;
         private System.Windows.Forms.TextBox Bit1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Bit8;
+        private System.Windows.Forms.TextBox Bit7;
+        private System.Windows.Forms.TextBox Bit6;
     }
 }
 
--- BinaryBoi/BinaryBoi/Form1.cs
+++ BinaryBoi/BinaryBoi/Form1.cs
@@ -13,35 +13,54 @@ namespace BinaryBoi
     public partial class Form1 : Form
     {
         int Total = 0;
+        int[] bits = new int[8];
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void Bit5_TextChanged(object sender, EventArgs e)
+        private void calculateValue()
         {
             Total = 0;
-            if (Bit1.Text == "1")
-            {
-                Total += 1;
-            }
-            if (Bit2.Text == "1") 
-            {
-                Total += 2;
-            }
-            if (Bit3.Text == "1")
-            {
-                Total += 4;
-            }
-            if (Bit4.Text == "1")
-            {
-                Total += 8;
-            }
-            if (Bit5.Text == "1")
-            {
-                Total += 16;
-            }
-            label1.Text = Total.ToString();
+
+            if (bits[0] != 0) Total += 1;
+            if (bits[1] != 0) Total += 2;
+            if (bits[2] != 0) Total += 4;
+            if (bits[3] != 0) Total += 8;
+            if (bits[4] != 0) Total += 16;
+            if (bits[5] != 0) Total += 32;
+            if (bits[6] != 0) Total += 64;
+            if (bits[7] != 0) Total += 128;
+           
+            label1.Text=Total.ToString();
+        }
+        private void Bit1_TextChanged(object sender, EventArgs e)
+        {
+            if (Bit1.Text == "1") bits[0] = 1;
+            else  bits[0] = 0;
+            //bit1
+            if (Bit2.Text == "1") bits[1] = 1;
+            else bits[1] = 0;
+            //bit2
+            if (Bit3.Text == "1") bits[2] = 1;
+            else bits[2] = 0;
+            //bit3
+            if (Bit4.Text == "1") bits[3] = 1;
+            else bits[3] = 0;
+            //bit4
+            if (Bit5.Text == "1") bits[4] = 1;
+            else bits[4] = 0;
+            //bit5
+            if (Bit6.Text == "1") bits[5] = 1;
+            else bits[5] = 0;
+            //bit6
+            if (Bit7.Text == "1") bits[6] = 1;
+            else bits[6] = 0;
+            //bit7
+            if (Bit8.Text == "1") bits[7] = 1;
+            else bits[7] = 0;
+            //bit8
+
+            calculateValue();
         }
     }
 }
