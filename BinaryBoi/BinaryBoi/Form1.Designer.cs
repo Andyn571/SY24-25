df9d34af197eda5fee2aabae9e3027c775a3c59a
FlatAsAPancake <ajkou571@learner.dvusd.org>
2025-01-14 09:25:53 -0700

    Even more Mental Abuse To Humans

--- BinaryBoi/BinaryBoi/Form1.Designer.cs
+++ BinaryBoi/BinaryBoi/Form1.Designer.cs
@@ -37,6 +37,24 @@
             this.Bit8 = new System.Windows.Forms.TextBox();
             this.Bit7 = new System.Windows.Forms.TextBox();
             this.Bit6 = new System.Windows.Forms.TextBox();
+            this.bBit6 = new System.Windows.Forms.TextBox();
+            this.bBit7 = new System.Windows.Forms.TextBox();
+            this.bBit8 = new System.Windows.Forms.TextBox();
+            this.bBit1 = new System.Windows.Forms.TextBox();
+            this.bBit2 = new System.Windows.Forms.TextBox();
+            this.bBit3 = new System.Windows.Forms.TextBox();
+            this.bBit4 = new System.Windows.Forms.TextBox();
+            this.bBit5 = new System.Windows.Forms.TextBox();
+            this.bBitt6 = new System.Windows.Forms.TextBox();
+            this.bBitt7 = new System.Windows.Forms.TextBox();
+            this.bBitt8 = new System.Windows.Forms.TextBox();
+            this.bBitt1 = new System.Windows.Forms.TextBox();
+            this.bBitt2 = new System.Windows.Forms.TextBox();
+            this.bBitt3 = new System.Windows.Forms.TextBox();
+            this.bBitt4 = new System.Windows.Forms.TextBox();
+            this.bBitt5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Bit5
@@ -102,7 +120,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(358, 302);
+            this.label1.Location = new System.Drawing.Point(564, 117);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 5;
@@ -144,11 +162,232 @@
             this.Bit6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Bit6.TextChanged += new System.EventHandler(this.Bit1_TextChanged);
             // 
+            // bBit6
+            // 
+            this.bBit6.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit6.Location = new System.Drawing.Point(150, 187);
+            this.bBit6.MaxLength = 1;
+            this.bBit6.Multiline = true;
+            this.bBit6.Name = "bBit6";
+            this.bBit6.Size = new System.Drawing.Size(63, 64);
+            this.bBit6.TabIndex = 16;
+            this.bBit6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit6.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit7
+            // 
+            this.bBit7.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit7.Location = new System.Drawing.Point(81, 187);
+            this.bBit7.MaxLength = 1;
+            this.bBit7.Multiline = true;
+            this.bBit7.Name = "bBit7";
+            this.bBit7.Size = new System.Drawing.Size(63, 64);
+            this.bBit7.TabIndex = 15;
+            this.bBit7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit7.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit8
+            // 
+            this.bBit8.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit8.Location = new System.Drawing.Point(12, 187);
+            this.bBit8.MaxLength = 1;
+            this.bBit8.Multiline = true;
+            this.bBit8.Name = "bBit8";
+            this.bBit8.Size = new System.Drawing.Size(63, 64);
+            this.bBit8.TabIndex = 14;
+            this.bBit8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit8.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit1
+            // 
+            this.bBit1.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit1.Location = new System.Drawing.Point(495, 187);
+            this.bBit1.MaxLength = 1;
+            this.bBit1.Multiline = true;
+            this.bBit1.Name = "bBit1";
+            this.bBit1.Size = new System.Drawing.Size(63, 64);
+            this.bBit1.TabIndex = 13;
+            this.bBit1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit1.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit2
+            // 
+            this.bBit2.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit2.Location = new System.Drawing.Point(426, 187);
+            this.bBit2.MaxLength = 1;
+            this.bBit2.Multiline = true;
+            this.bBit2.Name = "bBit2";
+            this.bBit2.Size = new System.Drawing.Size(63, 64);
+            this.bBit2.TabIndex = 12;
+            this.bBit2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit2.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit3
+            // 
+            this.bBit3.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit3.Location = new System.Drawing.Point(357, 187);
+            this.bBit3.MaxLength = 1;
+            this.bBit3.Multiline = true;
+            this.bBit3.Name = "bBit3";
+            this.bBit3.Size = new System.Drawing.Size(63, 64);
+            this.bBit3.TabIndex = 11;
+            this.bBit3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit3.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit4
+            // 
+            this.bBit4.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit4.Location = new System.Drawing.Point(288, 187);
+            this.bBit4.MaxLength = 1;
+            this.bBit4.Multiline = true;
+            this.bBit4.Name = "bBit4";
+            this.bBit4.Size = new System.Drawing.Size(63, 64);
+            this.bBit4.TabIndex = 10;
+            this.bBit4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit4.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBit5
+            // 
+            this.bBit5.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBit5.Location = new System.Drawing.Point(219, 187);
+            this.bBit5.MaxLength = 1;
+            this.bBit5.Multiline = true;
+            this.bBit5.Name = "bBit5";
+            this.bBit5.Size = new System.Drawing.Size(63, 64);
+            this.bBit5.TabIndex = 9;
+            this.bBit5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.bBit5.TextChanged += new System.EventHandler(this.bBit1_TextChanged);
+            // 
+            // bBitt6
+            // 
+            this.bBitt6.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt6.Location = new System.Drawing.Point(150, 257);
+            this.bBitt6.MaxLength = 1;
+            this.bBitt6.Multiline = true;
+            this.bBitt6.Name = "bBitt6";
+            this.bBitt6.Size = new System.Drawing.Size(63, 64);
+            this.bBitt6.TabIndex = 24;
+            this.bBitt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt7
+            // 
+            this.bBitt7.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt7.Location = new System.Drawing.Point(81, 257);
+            this.bBitt7.MaxLength = 1;
+            this.bBitt7.Multiline = true;
+            this.bBitt7.Name = "bBitt7";
+            this.bBitt7.Size = new System.Drawing.Size(63, 64);
+            this.bBitt7.TabIndex = 23;
+            this.bBitt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt8
+            // 
+            this.bBitt8.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt8.Location = new System.Drawing.Point(12, 257);
+            this.bBitt8.MaxLength = 1;
+            this.bBitt8.Multiline = true;
+            this.bBitt8.Name = "bBitt8";
+            this.bBitt8.Size = new System.Drawing.Size(63, 64);
+            this.bBitt8.TabIndex = 22;
+            this.bBitt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt1
+            // 
+            this.bBitt1.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt1.Location = new System.Drawing.Point(495, 257);
+            this.bBitt1.MaxLength = 1;
+            this.bBitt1.Multiline = true;
+            this.bBitt1.Name = "bBitt1";
+            this.bBitt1.Size = new System.Drawing.Size(63, 64);
+            this.bBitt1.TabIndex = 21;
+            this.bBitt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt2
+            // 
+            this.bBitt2.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt2.Location = new System.Drawing.Point(426, 257);
+            this.bBitt2.MaxLength = 1;
+            this.bBitt2.Multiline = true;
+            this.bBitt2.Name = "bBitt2";
+            this.bBitt2.Size = new System.Drawing.Size(63, 64);
+            this.bBitt2.TabIndex = 20;
+            this.bBitt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt3
+            // 
+            this.bBitt3.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt3.Location = new System.Drawing.Point(357, 257);
+            this.bBitt3.MaxLength = 1;
+            this.bBitt3.Multiline = true;
+            this.bBitt3.Name = "bBitt3";
+            this.bBitt3.Size = new System.Drawing.Size(63, 64);
+            this.bBitt3.TabIndex = 19;
+            this.bBitt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt4
+            // 
+            this.bBitt4.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt4.Location = new System.Drawing.Point(288, 257);
+            this.bBitt4.MaxLength = 1;
+            this.bBitt4.Multiline = true;
+            this.bBitt4.Name = "bBitt4";
+            this.bBitt4.Size = new System.Drawing.Size(63, 64);
+            this.bBitt4.TabIndex = 18;
+            this.bBitt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bBitt5
+            // 
+            this.bBitt5.AcceptsReturn = true;
+            this.bBitt5.Font = new System.Drawing.Font("Segoe Script", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBitt5.Location = new System.Drawing.Point(219, 257);
+            this.bBitt5.MaxLength = 1;
+            this.bBitt5.Multiline = true;
+            this.bBitt5.Name = "bBitt5";
+            this.bBitt5.Size = new System.Drawing.Size(63, 64);
+            this.bBitt5.TabIndex = 17;
+            this.bBitt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bBitt6);
+            this.Controls.Add(this.bBitt7);
+            this.Controls.Add(this.bBitt8);
+            this.Controls.Add(this.bBitt1);
+            this.Controls.Add(this.bBitt2);
+            this.Controls.Add(this.bBitt3);
+            this.Controls.Add(this.bBitt4);
+            this.Controls.Add(this.bBitt5);
+            this.Controls.Add(this.bBit6);
+            this.Controls.Add(this.bBit7);
+            this.Controls.Add(this.bBit8);
+            this.Controls.Add(this.bBit1);
+            this.Controls.Add(this.bBit2);
+            this.Controls.Add(this.bBit3);
+            this.Controls.Add(this.bBit4);
+            this.Controls.Add(this.bBit5);
             this.Controls.Add(this.Bit6);
             this.Controls.Add(this.Bit7);
             this.Controls.Add(this.Bit8);
@@ -176,6 +415,24 @@
         private System.Windows.Forms.TextBox Bit8;
         private System.Windows.Forms.TextBox Bit7;
         private System.Windows.Forms.TextBox Bit6;
+        private System.Windows.Forms.TextBox bBit6;
+        private System.Windows.Forms.TextBox bBit7;
+        private System.Windows.Forms.TextBox bBit8;
+        private System.Windows.Forms.TextBox bBit1;
+        private System.Windows.Forms.TextBox bBit2;
+        private System.Windows.Forms.TextBox bBit3;
+        private System.Windows.Forms.TextBox bBit4;
+        private System.Windows.Forms.TextBox bBit5;
+        private System.Windows.Forms.TextBox bBitt6;
+        private System.Windows.Forms.TextBox bBitt7;
+        private System.Windows.Forms.TextBox bBitt8;
+        private System.Windows.Forms.TextBox bBitt1;
+        private System.Windows.Forms.TextBox bBitt2;
+        private System.Windows.Forms.TextBox bBitt3;
+        private System.Windows.Forms.TextBox bBitt4;
+        private System.Windows.Forms.TextBox bBitt5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BinaryBoi/BinaryBoi/Form1.cs
+++ BinaryBoi/BinaryBoi/Form1.cs
@@ -14,6 +14,8 @@ namespace BinaryBoi
     {
         int Total = 0;
         int[] bits = new int[8];
+        int[] Bbits = new int[8];
+        int[] bitts = new int[8];
         public Form1()
         {
             InitializeComponent();
@@ -61,6 +63,49 @@ namespace BinaryBoi
             //bit8
 
             calculateValue();
+        }
+        private void calculateValue2()
+        {
+            Total = 0;
+
+            if (Bbits[0] != 0) Total += 1;
+            if (Bbits[1] != 0) Total += 2;
+            if (Bbits[2] != 0) Total += 4;
+            if (Bbits[3] != 0) Total += 8;
+            if (Bbits[4] != 0) Total += 16;
+            if (Bbits[5] != 0) Total += 32;
+            if (Bbits[6] != 0) Total += 64;
+            if (Bbits[7] != 0) Total += 128;
+
+            label2.Text = Total.ToString();
+        }
+        private void bBit1_TextChanged(object sender, EventArgs e)
+        {
+            if (bBit1.Text == "1") Bbits[0] = 1;
+            else Bbits[0] = 0;
+            //bit1
+            if (bBit2.Text == "1") Bbits[1] = 1;
+            else Bbits[1] = 0;
+            //bit2
+            if (bBit3.Text == "1") Bbits[2] = 1;
+            else Bbits[2] = 0;
+            //bit3
+            if (bBit4.Text == "1") Bbits[3] = 1;
+            else Bbits[3] = 0;
+            //bit4
+            if (bBit5.Text == "1") Bbits[4] = 1;
+            else Bbits[4] = 0;
+            //bit5
+            if (bBit6.Text == "1") Bbits[5] = 1;
+            else Bbits[5] = 0;
+            //bit6
+            if (bBit7.Text == "1") Bbits[6] = 1;
+            else Bbits[6] = 0;
+            //bit7
+            if (bBit8.Text == "1") Bbits[7] = 1;
+            else Bbits[7] = 0;
+            //bit8
+            calculateValue2();
         }
     }
 }
